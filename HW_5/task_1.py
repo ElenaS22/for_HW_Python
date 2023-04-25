b9de5f24114e5830d2227e9d58fe828640ab7837
Elena Sheveleva <lahjalena@gmail.com>
2023-04-25 10:48:06 +0200

    added tasks for HW_5

---
+++ HW_5/task_1.py
@@ -0,0 +1,22 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+# *Пример:*
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+
+def stepen(num_a, num_b):
+    # num_a = int(input("Ведите число A: "))
+    # num_b = int(input("Ведите число B: "))
+    if num_b == 1:
+        return num_a
+    elif num_b == 0:
+        return " 1 "
+    elif num_b > 0  and num_b != 1:
+        return (num_a * (stepen(num_a, num_b - 1)))
+    elif num_b < 0:
+        return " ошибка. Введите положительное число В"
+    
+a = int(input("Ведите число A: "))
+b = int(input("Ведите число B: "))
+print(f" Если возвести число {a} в степень {b} получится {stepen(a, b)}")
+
